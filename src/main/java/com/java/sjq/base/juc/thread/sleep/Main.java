cbf06a146ef24b2f01334d20af925b0192c1a20b
sunupo <sunupo@126.com>
2023-02-23 19:56:27 +0800

    feat: 线程代码中有sleep，被另一个线程interrupt的情况

--- src/main/java/com/java/sjq/base/juc/thread/sleep/Main.java
+++ src/main/java/com/java/sjq/base/juc/thread/sleep/Main.java
@@ -14,32 +14,42 @@ import static com.java.sjq.base.superclassStatic.P.a;
  */
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
+        interruptAfterSleep();
+        Thread.sleep(1000);
+//        sleepAfterInterrupted();
+    }
+
+    /**
+     * 1.先睡眠后打断，则直接打断睡眠，并且清除停止状态值，使之变成false：
+     */
+    public static void interruptAfterSleep(){
         try{
             MyThread thread=new MyThread();
             thread.start();
             Thread.sleep(10);
             thread.interrupt();
 
-            //Connecting to Redis server on localhost
-            Jedis jedis = new Jedis("localhost");
-            System.out.println("Connection to server sucessfully");
-            AtomicStampedReference atomicStampedReference;
-            //check whether server is running or not
-            jedis.set("name", "sunjingqin", "XX","EX", 1000 );
-            jedis.set("name", "sunjingqin2", "XX","EX", 1000 );
-            jedis.keys("*").stream().forEach((item)->{
-                System.out.println(item);
-                System.out.println(jedis.get(item));
-            });
-            System.out.println(""+jedis.ttl("name"));
-            System.out.println(jedis.get("name"));
-            System.out.println("Server is running: "+jedis.ping());
+//
+        }catch (InterruptedException e){
+            System.out.println("main catch");
+            e.printStackTrace();
+        }
+        System.out.println("Main end!");
+    }
 
-            int[] _int  = {1, 2, 9};
-            List ints = Arrays.asList(_int);
-            System.out.println(((int[]) (ints.get(0)))[2]);
-            System.out.println(ints.size());
+    /**
+     *2. 先打断后睡眠，则直接不睡眠：
+     * 运行到sleep接着立马抛出异常
+     */
+    public static void sleepAfterInterrupted(){
+        try{
+            MyThread2 thread=new MyThread2();
+            thread.start();
+            Thread.sleep(10);  // 确保thread启动了
+            thread.interrupt();
+
+//
         }catch (InterruptedException e){
             System.out.println("main catch");
             e.printStackTrace();
@@ -89,8 +99,9 @@ class MyThread2 extends Thread{
             }
             System.out.println("MyThread run 循环结束");
 
-            Thread.sleep(2000);
-            System.out.println("MyThread run end");
+            System.out.println("MyThread run end before");
+            Thread.sleep(2000);  // 在sleep之前已经被打断，于是不会睡眠，直接抛出InterruptedException
+            System.out.println("MyThread run end after");  //
         }catch (InterruptedException e){
             System.out.println("在沉睡中被停止！进入catch！"+this.isInterrupted());
             e.printStackTrace();
