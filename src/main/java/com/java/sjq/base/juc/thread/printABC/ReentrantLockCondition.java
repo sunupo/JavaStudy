b2def67b4a885ff2ce96dc8796b824cbad863f49
sunupo <sunupo@126.com>
2023-03-02 19:47:54 +0800

    feat: 线程交替打印

--- src/main/java/com/java/sjq/base/juc/thread/printABC/ReentrantLockCondition.java
+++ src/main/java/com/java/sjq/base/juc/thread/printABC/ReentrantLockCondition.java
@@ -1,2 +1,91 @@
-package com.java.sjq.base.juc.thread.printABC;public class ReentrantLockCondition {
+package com.java.sjq.base.juc.thread.printABC;
+
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReentrantLockCondition {
+    private static Lock lock = new ReentrantLock();
+    private static Condition A = lock.newCondition();
+    private static Condition B = lock.newCondition();
+    private static Condition C = lock.newCondition();
+    private static int state = 0;
+    public static void changeState(){
+        state++;
+        state%=3;
+    }
+    static class ThreadA extends Thread {
+        @Override
+        public void run() {
+            try {
+                lock.lock();
+                System.out.println("1");
+                for (int i = 0; i < 10; i++) {
+//              TODO  	为什么用while不用if：多线程并发，不能用if，必须用循环测试等待条件，避免虚假唤醒
+                    while (state != 0)//注意这里是不等于0，也就是说在state % 3为0之前，当前线程一直阻塞状态
+                        A.await(); // A释放lock锁
+                    System.out.print("A");
+                    changeState();
+                    B.signal(); // A执行完唤醒B线程
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
+    static class ThreadB extends Thread {
+        @Override
+        public void run() {
+            try {
+                lock.lock();
+                System.out.println("2");
+
+                for (int i = 0; i < 10; i++) {
+                    while (state != 1)
+                        B.await();// B释放lock锁，当前面A线程执行后会通过B.signal()唤醒该线程
+                    System.out.print("B");
+                    changeState();
+                    C.signal();// B执行完唤醒C线程
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
+    static class ThreadC extends Thread {
+        @Override
+        public void run() {
+            try {
+                lock.lock();
+                System.out.println("3");
+
+                for (int i = 0; i < 10; i++) {
+                    while (state != 2)
+                        C.await();// C释放lock锁
+                    System.out.print("C");
+                    changeState();
+                    A.signal();// C执行完唤醒A线程
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+    public static void main(String[] args) throws InterruptedException {
+        new ThreadB().start();
+        new ThreadC().start();
+
+        new ThreadA().start();
+
+
+    }
 }
--- src/main/java/com/java/sjq/base/juc/thread/printABC/SynchronizedDemo.java
+++ src/main/java/com/java/sjq/base/juc/thread/printABC/SynchronizedDemo.java
@@ -1,2 +1,74 @@
-package com.java.sjq.base.juc.thread.printABC;public class SynchronizedDemo {
+package com.java.sjq.base.juc.thread.printABC;
+
+
+/**
+ * 好像不太对
+ */
+public class SynchronizedDemo {
+
+    static String[] locks = {"locka","lockb","lockc","lockd"};// 每个线程对应一个锁标记。
+//	static String state="A";
+
+    static class MyThread implements Runnable{
+        String[] strings={"A","B","c","D"};
+        String str;
+        int[] idx = new int[2];
+        int cnt=3;
+        public MyThread(int i) {  //如果strings=[ABCD]。i=0，idx=[3,0]; i=1,idx=[0,1];i=2, idx=[1,2],i=3,idx=[2,3]
+            this.str = strings[i];
+            idx[0]=(i+strings.length-1)%strings.length;
+            idx[1]=i;
+            // TODO Auto-generated constructor stub
+        }
+        synchronized void decrease(){
+            cnt--;
+        }
+        @Override
+        public void run() {
+            while(cnt>0){
+                decrease();
+                synchronized (locks[idx[0]]) {  //locks[idx[0]]代表当前线程的前一个线程的锁标记
+                    synchronized (locks[idx[1]]) { // locks[idx[1]] 代表当前线程的锁标记
+                        System.out.println(str);
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        locks[idx[1]].notifyAll();
+                    }
+                    try {
+                        if (cnt>0)
+                            locks[idx[0]].wait();  //假如当前线程是A，locks[idx[0]]="lockc",只有C线程会调用"lockc".notify
+                        else
+                            locks[idx[0]].notifyAll();
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread ta = new Thread(new MyThread(0));
+        Thread tb = new Thread(new MyThread(1));
+        Thread tc = new Thread(new MyThread(2));
+        Thread td = new Thread(new MyThread(3));
+//		todo 必须保证t tb tc td依次被cpu调度依次过后，才能输出ABCDABCDABCD……
+        try {
+            ta.start();
+            Thread.sleep(500);
+            tb.start();
+            Thread.sleep(500);
+            tc.start();
+            Thread.sleep(500);
+            td.start();
+
+        } catch (InterruptedException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+    }
 }
