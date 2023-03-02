83253e0b754c5489bf7c21401a3bc0e0f7fc27eb
sunupo <sunupo@126.com>
2023-03-02 19:49:46 +0800

    feat: 自定义springmvc 类型转换器

--- src/main/java/com/soecode/lyf/convert/CustomDateConverter.java
+++ src/main/java/com/soecode/lyf/convert/CustomDateConverter.java
@@ -1,2 +1,21 @@
-package com.soecode.lyf.convert;public class CustomDateConverter {
+package com.soecode.lyf.convert;
+
+import org.springframework.core.convert.converter.Converter;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CustomDateConverter implements Converter<String, Date> {
+    @Override
+    public Date convert(String s) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try {
+            return  format.parse(s);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
 }
